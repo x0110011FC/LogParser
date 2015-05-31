33b58f17a46025479f20903c5255dc3d55369789
Aleks <Aleks@192.168.0.14>
2015-05-31 13:33:49 +0300

    Test commit

---
+++ TestAsMuchAsUNeed/src/test.java
@@ -0,0 +1,13 @@
+import java.awt.Panel;
+
+import javax.swing.InputMap;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Panel panel = new Panel();
+	}
+
+}
